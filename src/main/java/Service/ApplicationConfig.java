10a3930f5c0746b64cb8c2dd4048e75372990ca3
Osmar093 <iasc19854@isic.edu.mx>
2025-02-12 07:58:31 -0700

    actualizacion

--- src/main/java/Service/ApplicationConfig.java
+++ src/main/java/Service/ApplicationConfig.java
@@ -8,10 +8,7 @@ package Service;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
-/**
- *
- * @author Gilberto
- */
+
 @javax.ws.rs.ApplicationPath("api") // Puedes cambiar "api" si necesitas otra ruta
 public class ApplicationConfig extends Application {
 
@@ -23,14 +20,15 @@ public class ApplicationConfig extends Application {
     }
 
     /**
-     * No modificar el método addRestResourceClasses().
-     * Se llena automáticamente con todos los recursos REST en el proyecto.
+     * No modificar el mï¿½todo addRestResourceClasses().
+     * Se llena automï¿½ticamente con todos los recursos REST en el proyecto.
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(Service.Asesores.class);
         resources.add(Service.Clientes.class);
         resources.add(Service.CorsFilter.class);
         resources.add(Service.Login.class);
+        resources.add(Service.Pagos.class);
         resources.add(Service.Prestamos.class);
         resources.add(Service.UsuariosResource.class);
 
--- src/main/java/Service/HibernateUtil.java
+++ src/main/java/Service/HibernateUtil.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Service;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-/**
- *
- * @author Gilberto
- */
-class HibernateUtil {
+public class HibernateUtil {
     private static SessionFactory sessionFactory;
 
     static {
